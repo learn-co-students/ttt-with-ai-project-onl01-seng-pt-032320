a36a68c3b22a7692d52364387a90377f83af516e
HMoreira2020 <moreiraheather@gmail.com>
2020-05-03 16:05:54 -0700

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,6 +5,9 @@ module Players
             random = rand(1..9).to_s
             !board.taken?(random) ? random : move(board)
         end 
+
+        
+
     end 
 end 
 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -5,5 +5,19 @@ module Players
             puts "Please enter 1-9"
             input = gets.strip 
         end 
+
+        def corner 
+            #if move to one of the corners, code computer to move to middle 
+        end
+
+        def top 
+        #if move to index in bottom row, code comp to move to either side on bottom 
+        end 
+
+        def bottom 
+        #if move to index in top, code comp to move to either side on top 
+        end 
+
+
     end 
 end
