83b3a02b2c3028826eae0c9d10673ed66c7f59cd
HMoreira2020 <moreiraheather@gmail.com>
2020-05-06 18:52:24 -0700

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,23 +7,24 @@ module Players
         end
 
         def computer_move(board) 
-            middle_move(board) || corner_move(board) || empty_side_move(board) 
+            middle_move(board) || corner_move(board) || empty_side_move(board)
         end 
 
         def corner_move(board)
             corners = [1,3,7,9]
-            corners.detect{|corner| !board.taken?(corner)}.to_s
+            corners.detect{|corner| !board.taken?(corner)}
         end 
 
         def empty_side_move(board)
             sides = [2,4,6,8]
-            sides.detect{|side| !board.taken?(side)}.to_s
+            sides.detect{|side| !board.taken?(side)}
         end 
 
         def middle_move(board)
-            !board.taken?("5") ? "5" : corner_move(board)
+            if !board.taken?("5")
+                 "5" 
+            end 
         end 
-
 
     end 
 end 
